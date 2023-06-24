ec7f1f6a31db07f51b0df1d50eb0c0fc9ffa6376
Page Brooks <pagebrooks@gmail.com>
2023-06-23 17:15:45 -0700

    Fix bugs

--- .chezmoiscripts/run_once_00-debian-prerequisites.sh
+++ .chezmoiscripts/run_once_00-debian-prerequisites.sh
@@ -4,18 +4,17 @@
 sudo sed -i.bak -r '/deb cdrom:/s/^/#/' /etc/apt/sources.list 
 
 # Prepare clevis 
-sudo apt install -y \
-	curl \
-	clevis \
-	clevis-luks \
-	clevis-initramfs \
-	clevis-udisks2 \
-	clevis-systemd \
-	clevis-tpm2
+clevis=$(sudo cryptsetup luksDump /dev/nvme0n1p3 | grep clevis -c)
+if [[ $clevis -gt 0 ]]; then
+	exit
+fi
+
+sudo apt install -y curl clevis clevis-luks clevis-initramfs clevis-udisks2 clevis-systemd clevis-tpm2
 
 echo "Binding clevis to luks-encrypted volume"
-sudo clevis luks bind -d /dev/nvme0n1p3 tpm2 '{"pcr_bank":"sha_256","pcr_ids":"1,7"}'
+sudo clevis luks bind -d /dev/nvme0n1p3 tpm2 '{"pcr_bank":"sha256","pcr_ids":"1,7"}'
+sudo update-initramfs -u
 
 # References:
-https://kowalski7cc.xyz/blog/luks2-tpm2-clevis-fedora31/
-https://wiki.archlinux.org/title/Clevis
+# https://kowalski7cc.xyz/blog/luks2-tpm2-clevis-fedora31/
+# https://wiki.archlinux.org/title/Clevis
