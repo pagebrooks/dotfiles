98b8a1fe35787f76961af4f9836c3243f39fd70e
Page Brooks <pagebrooks@gmail.com>
2023-06-26 17:29:18 -0700

    Add obsidian

--- .chezmoiscripts/run_onchange_01-install-packages.sh
+++ .chezmoiscripts/run_onchange_01-install-packages.sh
@@ -12,11 +12,21 @@ sudo apt install -y \
 which 1password
 if [[ $? != 0 ]]; then
 	ONE_PASS=1password-latest.deb
-	curl https://downloads.1password.com/linux/debian/amd64/stable/$ONE_PASS --output /tmp/$ONE_PASS
+	curl -L -o /tmp/$ONE_PASS https://downloads.1password.com/linux/debian/amd64/stable/$ONE_PASS
 	sudo apt install -y /tmp/$ONE_PASS
 	rm /tmp/$ONE_PASS
 else
 	echo "1Password already installed"
+fi
+
+which obsidian
+if [[ $? != 0 ]]; then
+	OBSIDIAN_VERSION="1.3.5"
+	curl -L -o /tmp/obsidian.deb "https://github.com/obsidianmd/obsidian-releases/releases/download/v${OBSIDIAN_VERSION}/obsidian_${OBSIDIAN_VERSION}_amd64.deb"
+	sudo apt install -y /tmp/obsidian.deb
+	rm /tmp/obsidian.deb
+else
+	echo "Obsidian already installed"
 fi
 
 # Install Homebrew
